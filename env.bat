8fc523618d97a14f0333be55b6d246cd6982cd85
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2019-05-29 11:28:02 -0700

    Delete env.bat

--- env.bat
+++
@@ -1,4 +0,0 @@
-set MONGO_USERNAME=Mister-Bombastic
-set MONGO_PASSWORD=BIGGEY16
-set MONGO_HOST=mongodb+srv://Mister-Bombastic:BIGGEY16@cluster-test-vr2ad.mongodb.net/test?retryWrites=true
-set MONGO_DBNAME=Forum-Database
